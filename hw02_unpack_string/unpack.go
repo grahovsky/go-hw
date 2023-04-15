22dc710e2e28431844f1d5f74f158f1d634098be
Grakhovsky <grahovsky@gmail.com>
2023-04-15 23:47:08 +0300

    first impl

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,48 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+func Unpack(input string) (string, error) {
 	// Place your code here.
-	return "", nil
+	var bf strings.Builder
+
+	for i := 0; i < len([]rune(input))-1; i++ {
+
+		r := []rune(input)[i]
+		nr := []rune(input)[i+1]
+
+		if unicode.IsDigit(nr) {
+
+			if unicode.IsDigit(r) {
+				return "", ErrInvalidString
+			}
+
+			bf.WriteString(strings.Repeat(string(r), int(nr-'0')))
+
+		} else {
+
+			if i == len([]rune(input))-2 {
+				bf.WriteString(string(nr))
+			}
+
+			if unicode.IsDigit(r) {
+				if i == 0 {
+					return "", ErrInvalidString
+				}
+				continue
+			}
+
+			bf.WriteString(string(r))
+
+		}
+
+	}
+
+	return bf.String(), nil
+
 }
