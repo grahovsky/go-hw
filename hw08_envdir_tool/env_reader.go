7e80d765277057595b97710ef4701c12b6d79ca4
Grakhovsky <grahovsky@gmail.com>
2023-07-03 14:21:29 +0300

    hw08 code impl

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +16,49 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+const (
+	wrongEnvNameChars = "="
+	trimChars         = " \n\t"
+)
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	var env Environment = make(Environment)
+
+	for _, file := range files {
+		if !file.Mode().IsRegular() || file.Mode().IsDir() {
+			continue
+		}
+		if strings.ContainsAny(file.Name(), wrongEnvNameChars) {
+			continue
+		}
+		filename := filepath.Join(dir, file.Name())
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			return nil, err
+		}
+		fi, err := os.Stat(filename)
+		if err != nil {
+			return nil, err
+		}
+
+		env[strings.TrimRight(file.Name(), "=")] = EnvValue{
+			Value:      clearValue(data),
+			NeedRemove: fi.Size() == 0,
+		}
+
+	}
+	return env, nil
+}
+
+func clearValue(data []byte) string {
+	value := strings.Split(string(data), "\n")[0]
+	value = strings.TrimRight(value, trimChars)
+	value = string(bytes.ReplaceAll([]byte(value), []byte("\x00"), []byte("\n")))
+	return value
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Env = os.Environ()
+
+	for name, envVar := range env {
+		strEnvVar := fmt.Sprintf("%v=%v", name, envVar.Value)
+		switch envVar.NeedRemove {
+		case true:
+			command.Env = removeEnv(command.Env, name)
+		case false:
+			command.Env = append(command.Env, strEnvVar)
+		}
+	}
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	if err := command.Run(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			exitCode := exitErr.ExitCode()
+			return exitCode
+		} else {
+			log.Println(err)
+			return 1
+		}
+	}
+
+	return 0
+}
+
+func removeEnv(env []string, name string) []string {
+	result := make([]string, 0, len(env))
+	for _, item := range env {
+		if !strings.HasPrefix(item, name+"=") {
+			result = append(result, item)
+		}
+	}
+	return result
 }
