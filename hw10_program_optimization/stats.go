6f40941b651a905d717462e2d6b0f8811c8b45a5
Grakhovsky <grahovsky@gmail.com>
2023-07-12 16:55:03 +0300

    fix time used

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -2,24 +2,27 @@ package hw10programoptimization
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
 	"io"
 	"regexp"
 	"strings"
+
+	jsoniter "github.com/json-iterator/go"
 )
 
 type User struct {
+	Email    string
 	ID       int
 	Name     string
 	Username string
-	Email    string
 	Phone    string
 	Password string
 	Address  string
 }
 
 type DomainStat map[string]int
+
+var re = regexp.MustCompile(`.*@(.*)`)
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	u, err := getUsers(r)
@@ -32,6 +35,7 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 type users [100_000]User
 
 func getUsers(r io.Reader) (result users, err error) {
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
 	decoder := json.NewDecoder(bufio.NewReader(r))
 	i := 0
 	for decoder.More() {
@@ -49,15 +53,13 @@ func getUsers(r io.Reader) (result users, err error) {
 func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 
-	re, err := regexp.Compile(`.*@(.*.` + domain + `)`)
-	if err != nil {
-		return nil, err
-	}
-
 	for _, user := range u {
-		match := re.FindStringSubmatch(user.Email)
-		if len(match) > 0 {
-			result[strings.ToLower((match[1]))]++
+		if strings.HasSuffix(user.Email, domain) {
+			match := re.FindStringSubmatch(user.Email)
+			if len(match) > 0 {
+				lowerDomain := strings.ToLower(match[1])
+				result[lowerDomain]++
+			}
 		}
 	}
 
